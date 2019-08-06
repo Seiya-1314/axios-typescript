da4104fd3b1b9f1a75d8e975f9cda47bb74333a3
seiya <seiya.Zhufan@gmail.com>
2019-08-06 14:30:53 +0800

    feat: 新增headers、data转化

---
+++ src/helpers/headers.ts
@@ -0,0 +1,48 @@
+import { isPlainObject } from './util'
+
+/* 处理属性名的大小写情况，进行规范化 */
+function normalizeHeaderName(headers: any, normalizeName: string): void {
+  if (!headers) {
+    return
+  }
+
+  Object.keys(headers).forEach(key => {
+    if (key !== normalizeName && key.toUpperCase() === normalizeName.toUpperCase()) {
+      headers[normalizeName] = headers[key]
+      delete headers[key]
+    }
+  })
+}
+
+export function processHeaders(headers: any, data: any): any {
+  normalizeHeaderName(headers, 'Content-Type')
+
+  if (isPlainObject(data)) {
+    if (headers && !headers['Content-Type']) {
+      headers['Content-Type'] = 'application/json;charset=utf-8'
+    }
+  }
+
+  return headers
+}
+
+export function parseHeaders(headers: string): any {
+  let parsed = Object.create(null)
+  if (!headers) {
+    return parsed
+  }
+
+  headers.split('\r\n').forEach(line => {
+    let [key, val] = line.split(':')
+    key = key.trim().toLowerCase()
+    if (!key) {
+      return
+    }
+    if (val) {
+      val = val.trim()
+    }
+    parsed[key] = val
+  })
+
+  return parsed
+}
