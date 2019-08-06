303748d9ba4b242ae2d0f79ef3957a5095504227
seiya <seiya.Zhufan@gmail.com>
2019-08-06 14:31:21 +0800

    refactor: 代码重构

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -5,3 +5,8 @@ export function isDate(value: any): value is Date {
 export function isObject(value: any): value is Object {
   return value !== null && typeof value === 'object'
 }
+
+/* 普通对象类型判断 */
+export function isPlainObject(value: any): value is Object {
+  return Object.prototype.toString.call(value) === '[object Object]'
+}
