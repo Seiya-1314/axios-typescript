0975bbdd04ea20ae8a62dcfc015913447c239d3c
seiya <seiya.Zhufan@gmail.com>
2019-08-06 14:29:01 +0800

    refactor: 代码重构

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -3,71 +3,71 @@ import axios from '../../src/index'
 /* ------------- get 请求测试用例 --------------- */
 
 /* value 值为数组 */
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: ['bar', 'baz']
-  }
-})
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: ['bar', 'baz']
+//   }
+// })
 
-/* value 值为对象 */
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: {
-      bar: 'baz'
-    }
-  }
-})
+// /* value 值为对象 */
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: {
+//       bar: 'baz'
+//     }
+//   }
+// })
 
-/* value 值为 Date 对象 */
-const date = new Date()
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    date
-  }
-})
+// /* value 值为 Date 对象 */
+// const date = new Date()
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     date
+//   }
+// })
 
-/* value 值存在特殊字符 */
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: '@:$, '
-  }
-})
+// /* value 值存在特殊字符 */
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: '@:$, '
+//   }
+// })
 
-/* value 值中含有 null */
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: 'bar',
-    baz: null
-  }
-})
+// /* value 值中含有 null */
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: 'bar',
+//     baz: null
+//   }
+// })
 
-/* hash 路由 */
-axios({
-  method: 'get',
-  url: '/base/get#hash',
-  params: {
-    foo: 'bar'
-  }
-})
+// /* hash 路由 */
+// axios({
+//   method: 'get',
+//   url: '/base/get#hash',
+//   params: {
+//     foo: 'bar'
+//   }
+// })
 
-/* url 中带有参数 */
-axios({
-  method: 'get',
-  url: '/base/get?foo=bar',
-  params: {
-    bar: 'baz'
-  }
-})
+// /* url 中带有参数 */
+// axios({
+//   method: 'get',
+//   url: '/base/get?foo=bar',
+//   params: {
+//     bar: 'baz'
+//   }
+// })
 
 /* ------------- post 请求测试用例 --------------- */
 // axios({
@@ -99,7 +99,6 @@ axios({
 //   data: arr
 // })
 
-
 // const paramsString = 'q=URLUtils.searchParams&topic=api'
 // const searchParams = new URLSearchParams(paramsString)
 
@@ -109,25 +108,27 @@ axios({
 //   data: searchParams
 // })
 
-// axios({
-//   method: 'post',
-//   url: '/base/post',
-//   data: {
-//     a: 1,
-//     b: 2
-//   }
-// }).then((res) => {
-//   console.log(res)
-// })
+// tslint:disable-next-line: no-floating-promises
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: {
+    a: 1,
+    b: 2
+  }
+}).then(res => {
+  console.log(res)
+})
 
-// axios({
-//   method: 'post',
-//   url: '/base/post',
-//   responseType: 'json',
-//   data: {
-//     a: 3,
-//     b: 4
-//   }
-// }).then((res) => {
-//   console.log(res)
-// })
+// tslint:disable-next-line: no-floating-promises
+axios({
+  method: 'post',
+  url: '/base/post',
+  responseType: 'json',
+  data: {
+    a: 3,
+    b: 4
+  }
+}).then(res => {
+  console.log(res)
+})
