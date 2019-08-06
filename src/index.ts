3d34f0847a5fe1bdb464135b1123576fbfa0c9b7
seiya <seiya.Zhufan@gmail.com>
2019-08-06 14:35:33 +0800

    refactor: 代码重构

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,8 @@
 /**
  * 库入口文件
  */
-import { AxiosRequestConfig } from './types/index'
-import xhr from './xhr'
-import { buildURL } from "./helper/url";
+import axios from './axios'
 
-/**
- * @description 入口函数
- *
- * 通过 xhr 函数发送 http 请求
- *
- * @param config 请求配置
- */
-function axios(config: AxiosRequestConfig): void {
-  processConfig(config)
-  xhr(config)
-}
-
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformURL(config)
-}
-
-function transformURL(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return buildURL(url, params)
-}
+export * from './types'
 
 export default axios
